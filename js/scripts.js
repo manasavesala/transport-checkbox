43a878efab053604f422e6bdea945b5e0ac78295
maxwell dubin and manasa vesala <maxwell@>
2018-10-23 10:10:01 -0700

    Add a second survey category

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
+    $("#leisure-responses").show();
     var userResponses = [];
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
       userResponses.push(workTransportationMode);
-      alert(userResponses);
+    });
+      var leisureResponses = [];
+      $("input:checkbox[name=leisure-transportation]:checked").each(function(){
+        var leisureTransportationMode = $(this).val();
+        $('#leisure-responses').append(leisureTransportationMode + "<br>");
+        leisureResponses.push(leisureTransportationMode);
+
     });
     $('#transportation_survey').hide();
   });
